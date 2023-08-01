abe6b2aab8a09df834dc5c2cc67feb125f8c90e3
fuadsaft <fuadsafat16@gmail.com>
2023-08-01 20:11:42 +0600

    Add solution of Hackerrank Basic Python  Certification

---
+++ hackerrank certification/BasicPythonTest/ReverseWordSwapCase.py
@@ -0,0 +1,7 @@
+def swap_case(word):
+    return ''.join([character.lower() if character.isupper() else character.upper() for character in word])
+
+
+def reverse_words_order_and_swap_cases(sentence):
+    words = sentence.split(' ')[::-1]
+    return ' '.join([swap_case(word) for word in words])
